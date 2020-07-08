5d3539c48e326070557091a1df462c7843fc64c9
Choiseungheon <65004689+Choiseungheon@users.noreply.github.com>
2020-07-08 19:00:28 +0900

    Transfer Array Numbers

---
+++ 0708/05_study.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main() {
+
+	char ch[26] = { 'A','B','C','D','E','F','G','H','I',
+   'J','K','L','M','N','O','P','Q','R',
+   'S','T','U','V','W','X','Y','Z' };
+
+	char m = 0, n = 0, temp = 0, j = 25;
+
+
+	while (m < 26) {
+
+		n = 0;
+		while (n < 26) {
+
+			printf("%c ", ch[n]);
+			n++;
+		}
+		printf("\n");
+		n = 0;
+
+		while (n < 25) {
+
+			temp = ch[n];
+			ch[n] = ch[n + 1];
+			ch[n + 1] = temp;
+			n++;
+		}
+		m++;
+	}
+}
